2e3d0a7f4df9010235da6e2e791e83637f52150a
Marcos Antônio de Souza Silva <marcos@ssig.tech>
2025-02-09 11:21:56 -0300

    Add arquivo de strings das exceções

---
+++ lib/utils/strings/exceptions_string.dart
@@ -0,0 +1,7 @@
+class ExceptionsString {
+  static savePlaceException(String message) =>
+      'Erro ao salvar o local: $message';
+
+  static fetchPlacesException(String message) =>
+      'Erro ao buscar locais: $message';
+}
